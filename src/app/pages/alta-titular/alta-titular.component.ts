63c40dda7488b101b99293516404eeeb545df7eb
fbusso <fbusso@frsf.utn.edu.ar>
2020-10-19 23:05:05 -0300

    agregar funcion para convertir imagen

--- src/app/pages/alta-titular/alta-titular.component.ts
+++ src/app/pages/alta-titular/alta-titular.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm, FormGroup, FormControl, Validators, AbstractControl, ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { NgForm, FormGroup, FormControl, Validators, AbstractControl, ReactiveFormsModule, FormBuilder } from '@angular/forms';
 
 import { TipoDocumento } from '../../enums/tipo-documento.enum';
 import { GrupoSanguineo } from '../../enums/grupo-sanguineo.enum';
 import { FactorRH } from '../../enums/factor-rh.enum';
 import { ClaseLicencia } from 'src/app/enums/clase-licencia.enum';
+import { ReadVarExpr } from '@angular/compiler';
+import { resolve, Resolver } from 'dns';
+import { rejects } from 'assert';
 
 @Component({
   selector: 'app-alta-titular',
@@ -18,9 +21,9 @@ export class AltaTitularComponent implements OnInit {
   public FactorRH = FactorRH;
   public ClaseLicencia = ClaseLicencia;
 
-  altaTitularForm: FormGroup;
+  public altaTitularForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private cd: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     this.altaTitularForm = new FormGroup({
@@ -33,7 +36,7 @@ export class AltaTitularComponent implements OnInit {
       'grupoSanguineo': new FormControl(null, Validators.required),
       'factorRh': new FormControl(null, Validators.required),
       'condicionDonante': new FormControl(null, Validators.required),
-      //'foto': new FormControl(null, Validators.required),
+      'foto': new FormControl(null, Validators.required),
       'claseLicencia': new FormControl(null, Validators.required),
     });
 
@@ -49,12 +52,35 @@ export class AltaTitularComponent implements OnInit {
   get grupoSanguineo() { return this.altaTitularForm.get('grupoSanguineo'); }
   get factorRH() { return this.altaTitularForm.get('factorRh'); }
   get condicionDonante() { return this.altaTitularForm.get('condicionDonante'); }
-  //get foto() { return this.altaTitularForm.get('foto'); }
+  get foto() { return this.altaTitularForm.get('foto'); }
   get claseLicencia() { return this.altaTitularForm.get('claseLicencia'); }
 
   onSubmit(f: NgForm) {
     console.log(f.value);
   }
 
+  onFileSelected(event) {
+    let reader = new FileReader();
+    let file = event.target.files[0];
+    reader.readAsDataURL(file);
+    
+  
+    reader.onload = function () {
+      console.log(reader.result);
+      //altaTitularForm.get("foto").setValue(reader.result);
+    }    
+
+    //console.log(reader.result);
+  
+    reader.onerror = function (error) {
+      console.log('Error: ');
+    };
+ 
+
+
+  }
+
 
 }
+
+
