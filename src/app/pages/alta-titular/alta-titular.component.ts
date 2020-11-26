6bd57a53d194952920b87cf315bbe7cd0c6a9975
Giovani Ferraro Trivelli <gioxdvi@gmail.com>
2020-11-25 23:06:10 -0300

    Adding POST functionality

--- src/app/pages/alta-titular/alta-titular.component.ts
+++ src/app/pages/alta-titular/alta-titular.component.ts
@@ -5,6 +5,7 @@ import { GrupoSanguineo } from '../../enums/grupo-sanguineo.enum';
 import { FactorRH } from '../../enums/factor-rh.enum';
 import { ClaseLicencia } from 'src/app/enums/clase-licencia.enum';
 import { HelpersService } from '../../services/helpers/helpers.service';
+import { ApiService } from '../../services/api/api.service';
 
 @Component({
 	selector: 'app-alta-titular',
@@ -22,7 +23,8 @@ export class AltaTitularComponent implements OnInit {
 	public selectedImage;
 
 	constructor(
-		private helpersService : HelpersService
+		private helpersService : HelpersService,
+		private apiService : ApiService
 	) { }
 
 	ngOnInit(): void {
@@ -35,9 +37,9 @@ export class AltaTitularComponent implements OnInit {
 			'fechaNacimiento': new FormControl(null, Validators.required),
 			'grupoSanguineo': new FormControl(null, Validators.required),
 			'factorRh': new FormControl(null, Validators.required),
-			'condicionDonante': new FormControl(null, Validators.required),
+			'donante': new FormControl(null, Validators.required),
 			'foto': new FormControl(null, Validators.required),
-			'tipoLicencia': new FormControl(null, Validators.required),
+			'claseLicencia': new FormControl(null, Validators.required),
 			'observaciones': new FormControl(null),
 		});
 
@@ -46,19 +48,28 @@ export class AltaTitularComponent implements OnInit {
 	}
 
 	get nombre() { return this.altaTitularForm.get('nombre'); }
-	get apellidoTitular() { return this.altaTitularForm.get('apellido'); }
+	get apellido() { return this.altaTitularForm.get('apellido'); }
 	get domicilio() { return this.altaTitularForm.get('domicilio'); }
 	get tipoDocumento() { return this.altaTitularForm.get('tipoDocumento'); }
 	get nroDocumento() { return this.altaTitularForm.get('nroDocumento'); }
 	get fechaNacimiento() { return this.altaTitularForm.get('fechaNacimiento'); }
 	get grupoSanguineo() { return this.altaTitularForm.get('grupoSanguineo'); }
-	get factorRH() { return this.altaTitularForm.get('factorRh'); }
-	get condicionDonante() { return this.altaTitularForm.get('condicionDonante'); }
+	get factorRh() { return this.altaTitularForm.get('factorRh'); }
+	get donante() { return this.altaTitularForm.get('donante'); }
 	get foto() { return this.altaTitularForm.get('foto'); }
-	get tipoLicencia() { return this.altaTitularForm.get('tipoLicencia'); }
+	get claseLicencia() { return this.altaTitularForm.get('claseLicencia'); }
+	get observaciones() { return this.altaTitularForm.get('observaciones'); }
 
 	onSubmit(f: NgForm) {
-		console.log(f.value);
+		this.apiService.post('/api/alta-titular', f.value).subscribe(
+			loginResult => {
+				console.log(loginResult)
+			},
+			error => {
+				if(error.status == 403) {
+				}
+			}
+		);
 	}
 
 	onFileSelected(event: any) {
