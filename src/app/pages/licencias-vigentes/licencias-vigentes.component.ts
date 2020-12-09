92d5da476d8830df44af103c96be90d85b9c6f47
Francisco Busso <francisco.busso@outlook.com>
2020-12-09 18:24:47 -0300

    Agregar grupo sanguineo y factor RH en form de buscar licencias vigentes

--- src/app/pages/licencias-vigentes/licencias-vigentes.component.ts
+++ src/app/pages/licencias-vigentes/licencias-vigentes.component.ts
@@ -1,36 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm, FormGroup, FormControl, Validators, AbstractControl, ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { GrupoSanguineo } from "../../enums/grupo-sanguineo.enum";
+import { FactorRH } from "../../enums/factor-rh.enum";
+import {
+	NgForm,
+	FormGroup,
+	FormControl,
+	Validators,
+	AbstractControl,
+	ReactiveFormsModule,
+	FormBuilder,
+} from "@angular/forms";
 //import { Licencia } from '../../classes/'
 
 @Component({
-	selector: 'app-licencias-vigentes',
-	templateUrl: './licencias-vigentes.component.html',
-	styleUrls: ['./licencias-vigentes.component.scss']
+	selector: "app-licencias-vigentes",
+	templateUrl: "./licencias-vigentes.component.html",
+	styleUrls: ["./licencias-vigentes.component.scss"],
 })
 export class LicenciasVigentesComponent implements OnInit {
-
 	//private listaLicencias : Licencia[];
 	public buscarLicenciasForm: FormGroup;
 
-	public licencias : number[];
+	public licencias: number[];
+	public GrupoSanguineo = GrupoSanguineo;
+	public FactorRH = FactorRH;
 
-	constructor() { }
+	constructor() {}
 
 	ngOnInit(): void {
 		this.buscarLicenciasForm = new FormGroup({
-			'nombre': new FormControl(null, Validators.required),
-			'apellido': new FormControl(null, Validators.required),
-			'grupoSanguineo': new FormControl(null, Validators.required),
-			'factorRh': new FormControl(null, Validators.required),
-			'donante': new FormControl(null, Validators.required)
+			nombre: new FormControl(null, Validators.required),
+			apellido: new FormControl(null, Validators.required),
+			grupoSanguineo: new FormControl(null, Validators.required),
+			factorRh: new FormControl(null, Validators.required),
+			donante: new FormControl(null, Validators.required),
 		});
 	}
 
-	get nombre() { return this.buscarLicenciasForm.get('nombre'); }
-	get apellido() { return this.buscarLicenciasForm.get('apellido'); }
-	get grupoSanguineo() { return this.buscarLicenciasForm.get('grupoSanguineo'); }
-	get factorRh() { return this.buscarLicenciasForm.get('factorRh'); }
-	get donante() { return this.buscarLicenciasForm.get('donante'); }
+	get nombre() {
+		return this.buscarLicenciasForm.get("nombre");
+	}
+	get apellido() {
+		return this.buscarLicenciasForm.get("apellido");
+	}
+	get grupoSanguineo() {
+		return this.buscarLicenciasForm.get("grupoSanguineo");
+	}
+	get factorRh() {
+		return this.buscarLicenciasForm.get("factorRh");
+	}
+	get donante() {
+		return this.buscarLicenciasForm.get("donante");
+	}
 
 	onSubmit(f: NgForm) {
 		console.log(f.value);
