9af204ec559c02cbcf038093cee883183876ce33
Francisco Busso <francisco.busso@outlook.com>
2020-12-12 22:28:20 -0300

    Fix endpoint licencias vigentes

--- src/app/pages/licencias-vigentes/licencias-vigentes.component.ts
+++ src/app/pages/licencias-vigentes/licencias-vigentes.component.ts
@@ -1,80 +1,91 @@
-import { Component, OnInit } from "@angular/core";
-import { GrupoSanguineo } from "../../enums/grupo-sanguineo.enum";
-import { FactorRH } from "../../enums/factor-rh.enum";
-import { ApiService } from "../../services/api/api.service";
+import { Component, OnInit } from '@angular/core';
+import { GrupoSanguineo } from '../../enums/grupo-sanguineo.enum';
+import { FactorRH } from '../../enums/factor-rh.enum';
+import { ApiService } from '../../services/api/api.service';
 import {
-	NgForm,
-	FormGroup,
-	FormControl,
-	Validators,
-	AbstractControl,
-	ReactiveFormsModule,
-	FormBuilder,
-} from "@angular/forms";
+  NgForm,
+  FormGroup,
+  FormControl,
+  Validators,
+  AbstractControl,
+  ReactiveFormsModule,
+  FormBuilder,
+} from '@angular/forms';
 
 //import { Licencia } from '../../classes/'
 
 @Component({
-	selector: "app-licencias-vigentes",
-	templateUrl: "./licencias-vigentes.component.html",
-	styleUrls: ["./licencias-vigentes.component.scss"],
+  selector: 'app-licencias-vigentes',
+  templateUrl: './licencias-vigentes.component.html',
+  styleUrls: ['./licencias-vigentes.component.scss'],
 })
 export class LicenciasVigentesComponent implements OnInit {
-	//private listaLicencias : Licencia[];
-	public buscarLicenciasForm: FormGroup;
+  //private listaLicencias : Licencia[];
+  public buscarLicenciasForm: FormGroup;
 
-	public licencias: number[];
-	public GrupoSanguineo = GrupoSanguineo;
-	public FactorRH = FactorRH;
-	private params = "";
+  public licencias: number[];
+  public GrupoSanguineo = GrupoSanguineo;
+  public FactorRH = FactorRH;
+  private params = '';
 
-	constructor(private apiService: ApiService) {}
+  constructor(private apiService: ApiService) {}
 
-	ngOnInit(): void {
-		this.buscarLicenciasForm = new FormGroup({
-			nombre: new FormControl(null),
-			apellido: new FormControl(null),
-			grupoSanguineo: new FormControl(null),
-			factorRh: new FormControl(null),
-			donante: new FormControl(null),
-		});
-	}
+  ngOnInit(): void {
+    this.buscarLicenciasForm = new FormGroup({
+      nombre: new FormControl(null),
+      apellido: new FormControl(null),
+      grupoSanguineo: new FormControl(null),
+      factorRh: new FormControl(null),
+      donante: new FormControl(null),
+    });
+  }
 
-	get nombre() {
-		return this.buscarLicenciasForm.get("nombre");
-	}
-	get apellido() {
-		return this.buscarLicenciasForm.get("apellido");
-	}
-	get grupoSanguineo() {
-		return this.buscarLicenciasForm.get("grupoSanguineo");
-	}
-	get factorRh() {
-		return this.buscarLicenciasForm.get("factorRh");
-	}
-	get donante() {
-		return this.buscarLicenciasForm.get("donante");
-	}
+  get nombre() {
+    return this.buscarLicenciasForm.get('nombre');
+  }
+  get apellido() {
+    return this.buscarLicenciasForm.get('apellido');
+  }
+  get grupoSanguineo() {
+    return this.buscarLicenciasForm.get('grupoSanguineo');
+  }
+  get factorRh() {
+    return this.buscarLicenciasForm.get('factorRh');
+  }
+  get donante() {
+    return this.buscarLicenciasForm.get('donante');
+  }
 
-	onSubmit(f: NgForm) {
-		let params = new URLSearchParams();
+  onSubmit(f: NgForm) {
+    let params = new URLSearchParams();
 
-		for(let key in f.value){
-			params.set(key, f.value[key]);
-		}
+    for (let key in f.value) {
+      if (f.value[key] == 'null') params.set(key, f.value[key]);
+    }
 
-		this.apiService.get('/api/v1.0/licencias?' + params.toString()).subscribe(
-			(loginResult) => {
-				console.log(loginResult);
-			},
-			(error) => {
-				if (error.status == 403) {
-				}
-			}
-		);
-	}
+    console.log(params.toString());
 
-	getLicencias() {
-		return this.licencias;
-	}
+    this.apiService.get('/api/licencias?' + params.toString()).subscribe(
+      //this.apiService.get('/api/licencias?').subscribe(
+      // para testear si hay licencias
+      (result) => {
+        console.log(result.body);
+        console.log(result.body['cantidadResultadosTotales']);
+      },
+      (error) => {
+        switch (error.status) {
+          case 403:
+            console.log('FORBIDDEN');
+            break;
+          case 400:
+            console.log('BAD REQUEST');
+            break;
+        }
+      }
+    );
+  }
+
+  getLicencias() {
+    return this.licencias;
+  }
 }
