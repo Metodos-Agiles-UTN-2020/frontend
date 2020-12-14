b2c2b0fc05e3204a10f19fe0920a6f935de1cc41
Giovani Ferraro Trivelli <gioxdvi@gmail.com>
2020-12-14 20:43:02 -0300

    Alta usuario

--- src/app/pages/alta-usuario/alta-usuario.component.ts
+++ src/app/pages/alta-usuario/alta-usuario.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { NgForm, FormGroup, FormControl, Validators, AbstractControl, ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { TipoDocumento } from '../../enums/tipo-documento.enum';
+import { GrupoSanguineo } from '../../enums/grupo-sanguineo.enum';
+import { FactorRH } from '../../enums/factor-rh.enum';
+import { ClaseLicencia } from 'src/app/enums/clase-licencia.enum';
+import { HelpersService } from '../../services/helpers/helpers.service';
+import { ApiService } from '../../services/api/api.service';
 
 @Component({
   selector: 'app-alta-usuario',
@@ -6,10 +13,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alta-usuario.component.scss']
 })
 export class AltaUsuarioComponent implements OnInit {
+	constructor(private apiService: ApiService) {}
 
-  constructor() { }
+	public TipoDocumento = TipoDocumento;
+	public altaUsuarioForm: FormGroup;
+	public displayWaitMessage = false;
+	public displayForm = false;
+	public displayNoResultMessage = false;
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.altaUsuarioForm = new FormGroup({
+			username: new FormControl(null, Validators.required),
+			password: new FormControl(null, Validators.required),
+			nombre: new FormControl(null, Validators.required),
+			apellido: new FormControl(null, Validators.required),
+			mail: new FormControl(null, Validators.required),
+			tipoUsuario: new FormControl("ADMINISTRATIVO", Validators.required),
+			dni: new FormControl(null, Validators.required),
+		});
+	}
 
+	get username() { return this.altaUsuarioForm.get('username'); }
+	get password() { return this.altaUsuarioForm.get('password'); }
+	get nombre() { return this.altaUsuarioForm.get('nombre'); }
+	get apellido() { return this.altaUsuarioForm.get('apellido'); }
+	get mail() { return this.altaUsuarioForm.get('mail'); }
+	get dni() { return this.altaUsuarioForm.get('dni'); }
+	get tipoUsuario() { return this.altaUsuarioForm.get('tipoUsuario'); }
+
+	altaUsuario(f: NgForm) {
+		this.apiService.post('/api/user', f.value).subscribe(
+			(result) => {
+			},
+			(error) => {
+			}
+		);
+	}
 }
